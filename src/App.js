7d41fb60388e7859b60169cfb56f5a4f5992acaf
Ranjith428 <ranjithranjith67685@gmail.com>
2024-12-14 14:50:46 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Welcome from "./welcome";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Welcome />
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see magic happen!</h2>
+    </div>
+  );
+}
---
+++ src/welcome.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function Welcome() {
+  return (
+    <div className="Apple">
+      <h1>Hello </h1>
+      <h2>Start editing </h2>
+    </div>
+  );
+}
